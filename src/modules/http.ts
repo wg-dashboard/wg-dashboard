a6db2de2711d3ad4d4ca19ec502534faea71ca93
daluf <42932723+daluf@users.noreply.github.com>
2020-02-04 13:51:40 +0100

    fix tsconfig, make express listen

--- src/modules/http.ts
+++ src/modules/http.ts
@@ -3,22 +3,32 @@ import morgan from "morgan";
 
 class webServer {
 	private app: Express;
+	private initialized = false;
 
 	constructor() {
 		this.app = express();
 	}
 
 	public init = () => {
-		this.app.use(morgan("dev"));
-		this.app.use(express.json());
+		if (!this.initialized) {
+			this.app.use(morgan("dev"));
+			this.app.use(express.json());
 
-		this.app.get("/", (req, res) => {
-			res.send("Hello there!");
-		});
+			this.app.get("/", (req, res) => {
+				res.send("Hello there!");
+			});
 
-		this.app.use((req, res) => {
-			res.send("404");
-		});
+			this.app.use((req, res) => {
+				res.send("404 - not found");
+			});
+
+			this.app.listen(3000, () => {
+				this.initialized = true;
+				console.log(
+					"WG-Dashboard webserver now listening on port 3000!"
+				);
+			});
+		}
 	};
 }
 
