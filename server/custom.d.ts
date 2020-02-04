6c1c1e59257c25a61a19e3547113f4e05114f5c0
daluf <42932723+daluf@users.noreply.github.com>
2020-02-04 16:01:23 +0100

    add testing endpoints

---
+++ server/custom.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+	export interface Request {
+		authed?: boolean;
+	}
+}
--- server/modules/http.ts
+++ server/modules/http.ts
@@ -1,4 +1,4 @@
-import express, {Express} from "express";
+import express, {Express, Request, Response, NextFunction} from "express";
 import next from "next";
 import nextServer from "next/dist/next-server/server/next-server";
 import morgan from "morgan";
@@ -18,6 +18,18 @@ class WebServer {
 		this.server.use(express.json());
 	}
 
+	public isUserAuthed = (
+		req: Request,
+		_res: Response,
+		next: NextFunction
+	) => {
+		if (req.authed) {
+			return next(new Error("User not authenticated"));
+		}
+
+		next();
+	};
+
 	public init = () => {
 		if (this.initialized) {
 			return console.error("WebServer already initialized!");
@@ -26,9 +38,27 @@ class WebServer {
 		this.app.prepare().then(() => {
 			const handle = this.app.getRequestHandler();
 
-			this.server.get("*", (req, res) => {
-				handle(req, res);
+			this.server.get("/users", this.isUserAuthed, (req, res) => {
+				res.send({status: 200, message: req.authed});
 			});
+
+			this.server.get("/api/ping", (_req, res) =>
+				res.send({status: 200, message: "pong"})
+			);
+
+			this.server.get(
+				"*",
+				(
+					req: Request,
+					res: Response,
+					_next: NextFunction,
+					err?: Function
+				) => {
+					console.log(err);
+
+					handle(req, res);
+				}
+			);
 
 			this.server.listen(this.port, () => {
 				this.initialized = true;
