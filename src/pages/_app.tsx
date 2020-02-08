ba828316d19e2a2aeaa864806cff1640f2303b4a
vtii <vtmucode@gmail.com>
2020-02-08 22:27:36 +0100

    update primary color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,7 @@ import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: "#757ce8",
-			main: "#3f50b5",
-			dark: "#002884",
+			main: "#2d619b",
 			contrastText: "#fff",
 		},
 		secondary: {
